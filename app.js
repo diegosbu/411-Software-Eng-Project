21985f6e6ee988078d8ce1c28b9e2cba099ded1d
diegosbu <diegos@bu.edu>
2021-11-11 17:13:41 -0500

    Nodejs backend initialized/Index page added

---
+++ app.js
@@ -0,0 +1,18 @@
+// Module Dependencies 
+const express = require('express');
+const app = express();
+
+app.use(express.static('views'));
+
+
+// Routes
+
+app.get('/', function (req, res) {
+    res.render(index);
+})
+
+// Server
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
