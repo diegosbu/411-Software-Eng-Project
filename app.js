e6edff7d96cde5a93385bfc0b2cb0cd0f656714b
diegosbu <diegos@bu.edu>
2021-11-12 15:13:59 -0500

    Simple HTML form for restaurant search added

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express();
 
 app.use(express.static('views'));
 
-
 // Routes
 
 app.get('/', function (req, res) {
     res.render(index);
 })
 
+
 // Server
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT)
